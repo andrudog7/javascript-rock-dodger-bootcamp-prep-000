bdd2a20f1cab7983f565fe7911d72765ee66a72e
Andrew Smoker <andrewksmoker@gmail.com>
2020-10-06 15:47:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,17 +36,23 @@ function checkCollision(rock) {
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
     const rockRightEdge = rockLeftEdge + 20;
 
-    if (false /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge.
-               */) {
-      return true
+    if (rockLeftEdge <= dodgerLeftEdge) and (rockRightEdge >= dodgerLeftEdge)
+    true;
+    else if (rockLeftEdge >= dodgerLeftEdge) and (rockRightEdge <= dodgerRightEdge)
+    true
+    else if (rockLeftEdge <= dodgerRightEdge) and (rockRightEdge > dodgerRightEdge)
+    true
+    else 
+    false
+              // * Think about it -- what's happening here?
+              // * There's been a collision if one of three things is true:
+            //   * 1. The rock's left edge is < the DODGER's left edge,
+              // *    and the rock's right edge is > the DODGER's left edge;
+              // * 2. The rock's left edge is > the DODGER's left edge,
+              // *    and the rock's right edge is < the DODGER's right edge;
+             //  * 3. The rock's left edge is < the DODGER's right edge,
+             //  *    and the rock's right edge is > the DODGER's right edge.
+             //  */) {
     }
   }
 }
