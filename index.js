b81525ffaf5bae1cea132da8c7868133e19ef70e
Andrew Smoker <andrewksmoker@gmail.com>
2020-10-06 16:05:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -113,6 +113,15 @@ return endGame();}
  */
 function endGame() {
   clearInterval(gameInterval)
+
+  ROCKS.forEach(function(rock) { rock.remove() })
+
+  document.removeEventListener('keydown', moveDodger)
+
+  START.innerHTML = 'Play again?'
+  START.style.display = 'inline'
+
+  return alert('YOU LOSE!')
 }
 
 function moveDodger(e) {
