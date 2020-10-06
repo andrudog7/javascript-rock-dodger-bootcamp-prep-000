895c1802c6de0b4fe259e5b8e980ef97dba8a07a
Andrew Smoker <andrewksmoker@gmail.com>
2020-10-06 16:14:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  window.requestAnimationFrame(function()) {
+  window.requestAnimationFrame(function() {
     const right = positionToInteger(DODGER.style.right)
     if (right > 0) {
       DODGER.style.right = `${right + 4}px`;
